738121827f0b435fb34b6628a3c074a5013f5f32
原田光一 <haradakouichi@KoMacBook-Pro.local>
2023-06-28 12:21:35 +0900

    修正

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const nurseriesRouter = require('./routes/nurseries');
 const favoritesRouter = require('./routes/favorites');
 
 app.use(cors({
-  origin: 'https://hoiku-front1.vercel.app/', //アクセス許可するオリジン
+  origin: 'https://hoiku-front1.vercel.app', //アクセス許可するオリジン
   credentials: true, //レスポンスヘッダーにAccess-Control-Allow-Credentials追加
   optionsSuccessStatus: 200 //レスポンスstatusを200に設定
 }))
