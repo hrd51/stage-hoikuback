a8a83ee00cd25b7b8bc0e2c0682849a8eef0c2d9
原田光一 <haradakouichi@KoMacBook-Pro.local>
2023-06-29 19:19:45 +0900

    cors2修正2

--- server.js
+++ server.js
@@ -4,6 +4,23 @@ const cors = require('cors');
 const app = express();
 const sequelize = require('./sequelize'); // Use the sequelize instance from sequelize.js
 
+app.use(cors({
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'https://hoiku-front1.vercel.app',
+      // 他の許可するオリジンを追加
+    ];
+    // originが許可するリストに存在しない場合、CORSエラーを生成
+    if (!origin || allowedOrigins.indexOf(new URL(origin).origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('CORS policy: This origin is not allowed'))
+    }
+  },
+  credentials: true,
+}))
+
+
 console.log("process.env", process.env.DATABASE_URL );
 console.log("sequelize instance: ", sequelize);
 
@@ -18,22 +35,6 @@ sequelize.authenticate()
 const nurseriesRouter = require('./routes/nurseries');
 const favoritesRouter = require('./routes/favorites');
 
-app.use(cors({
-  origin: function (origin, callback) {
-    const allowedOrigins = [
-      'https://hoiku-front1.vercel.app',
-      'https://hoiku-front1.vercel.app/api/favorites'
-      // 他の許可するオリジンを追加
-    ];
-    // originが許可するリストに存在しない場合、CORSエラーを生成
-    if (!origin || allowedOrigins.indexOf(new URL(origin).origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error('CORS policy: This origin is not allowed'))
-    }
-  },
-  credentials: true,
-}))
 
 app.use(express.json());
 
