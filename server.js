1aed7f792b306a3c94c6b02eaf8f8cd1c4d9655d
原田光一 <haradakouichi@KoMacBook-Pro.local>
2023-06-29 18:58:06 +0900

    cors2種類にした

--- server.js
+++ server.js
@@ -19,9 +19,22 @@ const nurseriesRouter = require('./routes/nurseries');
 const favoritesRouter = require('./routes/favorites');
 
 app.use(cors({
-  origin: 'https://hoiku-front1.vercel.app',
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      'https://hoiku-front1.vercel.app',
+      'https://hoiku-front1.vercel.app/?code=l2Le745TzhYLin1q9JBG&state=3WclQ28fxzNo&liffClientId=1661542735&liffRedirectUri=https%3A%2F%2Fhoiku-front1.vercel.app%2F&liffIsEscapedFromApp=true'
+      // 他の許可するオリジンを追加
+    ];
+    // originが許可するリストに存在しない場合、CORSエラーを生成
+    if (!origin || allowedOrigins.indexOf(new URL(origin).origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('CORS policy: This origin is not allowed'))
+    }
+  },
   credentials: true,
 }))
+
 app.use(express.json());
 
 app.use('/api/nurseries', nurseriesRouter);
