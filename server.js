970a9c65488893878f552bb6d5e8117266f978fc
原田光一 <haradakouichi@KoMacBook-Pro.local>
2023-06-29 16:44:04 +0900

    sequelize.jsを作った

--- server.js
+++ server.js
@@ -1,21 +1,11 @@
 require('dotenv').config();
-
 const express = require('express');
 const cors = require('cors');
-console.log("process.env", process.env.DATABASE_URL );
-
 const app = express();
-const { Sequelize } = require('sequelize');
+const sequelize = require('./sequelize'); // Use the sequelize instance from sequelize.js
 
-// PostgreSQLへの接続設定
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-  protocol: 'postgres',
-  dialectOptions: {},
-  logging: false,
-});
-
-console.log("sequelize instance: ", sequelize); // Add this line to check the sequelize instance
+console.log("process.env", process.env.DATABASE_URL );
+console.log("sequelize instance: ", sequelize);
 
 sequelize.authenticate()
   .then(() => {
@@ -25,12 +15,11 @@ sequelize.authenticate()
     console.error('Unable to connect to PostgreSQL:', err);
   });
 
-// Now that sequelize instance is created, import the routers
 const nurseriesRouter = require('./routes/nurseries');
 const favoritesRouter = require('./routes/favorites');
 
 app.use(cors({
-  origin: 'https://hoiku-front1.vercel.app', 
+  origin: 'https://hoiku-front1.vercel.app',
   credentials: true,
 }))
 app.use(express.json());
@@ -43,5 +32,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-module.exports.sequelize = sequelize; // Fix this line to export the sequelize instance correctly
