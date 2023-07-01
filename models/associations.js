46d5c81a2897f2c77ab8f3cdb08216ae22aa6c52
keisuke ohashi <kog71903@gmail.com>
2023-07-01 15:26:37 +0900

    delete models

--- models/associations.js
+++
@@ -1,7 +0,0 @@
-const Nursery = require('./nursery');
-const Favorite = require('./favorite');
-
-module.exports = () => {
-  Favorite.belongsTo(Nursery, { foreignKey: 'nursery_id' });
-  Nursery.hasMany(Favorite, { foreignKey: 'nursery_id' });
-};
